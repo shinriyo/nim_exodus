d13821805ad47c241c420d5e5353f458c9467953
shinriyo <shinriyo@gmail.com>
2018-03-13 15:30:40 +0900

    update readme and some

--- README.md
+++ README.md
@@ -22,3 +22,22 @@ It is image of scadfdolding.
 ---
 nim c -r json_app.nim
 ---
+
+* curl
+
+- select
+
+    curl localhost:5000/users
+
+- insert
+
+    curl -F name=test localhost:5000/users
+
+- update
+
+    curl -X PATCH -F name=hoge localhost:5000/users/1
+
+- delete
+
+    curl -X PATCH -F name=hoge localhost:5000/users/1
+
--- sample/sqlite_controller.nim
+++ sample/sqlite_controller.nim
@@ -1,56 +1,29 @@
 import db_sqlite
 import json
 
-proc insert() =
-  block:
-    let db = open("mydb.db","user","password","dbname")
-    let ddl = @[
-      sql"""drop table work""",
-      sql"""create table work (id integer primary key autoincrement,name varchar(50) not null)"""
-    ]
-    defer:
-      echo "db closed"
-      db.close()
-    # DDLを実行
-    for cmd in ddl :
-      discard db.tryExec(cmd)
-    # レコードを挿入
-    db.exec(sql"BEGIN")
-    # 追加したれコードに自動採番された値を取り出す
-    echo "追加されたID=",db.tryInsertID(sql"insert into work(name) values(?)","Mr.スポック" )
-    echo "追加されたID=",db.tryInsertID(sql"insert into work(name) values(?)","Mr.ビーン" )
-    echo "追加されたID=",db.tryInsertID(sql"insert into work(name) values(?)","Mr.サタン" )
-    echo "更新された行=",db.execAffectedRows(sql"update work set name=? where id=?","Mr.ホームズ",3)
-    db.exec(sql"COMMIT")
+let db = open("mydb.db","user","password","dbname")
 
 proc select(id: string): JsonNode =
-  block:
-    let db = open("mydb.db","user","password","dbname")
-
-    # select
-    var row: Row = db.getRow(sql"select * from work where id = ?", id)
-    var data = %*{"id": row[0], "name": row[1]}
-    return data
+  # select
+  var row: Row = db.getRow(sql"select * from work where id = ?", id)
+  var data = %*{"id": row[0], "name": row[1]}
+  return data
 
 proc selectAll(): seq[JsonNode] =
-  block:
-    let db = open("mydb.db","user","password","dbname")
-    var rows: seq[Row] = db.getAllRows(sql"select * from work")
-    var users: seq[JsonNode]
-    users = @[]
-    for row in rows:
-      users.add(%*{"id": row[0], "name": row[1]})
-    # select
-    return users
+  var rows: seq[Row] = db.getAllRows(sql"select * from work")
+  var users: seq[JsonNode]
+  users = @[]
+  for row in rows:
+    users.add(%*{"id": row[0], "name": row[1]})
+  # select
+  return users
 
 proc insert(name: string): JsonNode =
-    let db = open("mydb.db","user","password","dbname")
-    var id = db.tryInsertId(sql"insert into work (name) values (?)", name)
-    var data = %*{"id": id, "name": name}
-    return data
+  var id = db.tryInsertId(sql"insert into work (name) values (?)", name)
+  var data = %*{"id": id, "name": name}
+  return data
 
 proc update(id: string, name: string): JsonNode =
-  let db = open("mydb.db","user","password","dbname")
   db.exec(sql"update work set name = ? where id = ?", name, id)
   var data = %*{"id": id, "name": name}
   return data
@@ -62,9 +35,6 @@ proc delete() =
       sql"""drop table work""",
       sql"""create table work (id integer primary key autoincrement,name varchar(50) not null)"""
     ]
-    defer:
-      echo "db closed"
-      db.close()
     # DDLを実行
     for cmd in ddl :
       discard db.tryExec(cmd)
