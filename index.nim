e3fb1fb4177947b894cb74c3a86e2065d1b649f5
shinriyo <shinriyo@gmail.com>
2018-03-17 16:18:29 +0900

    fieldName

--- index.nim
+++ index.nim
@@ -3,7 +3,7 @@ import strutils
 
 type
   Field* = object
-    name*, fieldType: string
+    fieldName*, fieldType*: string
 
 proc scaffoldiong(modelName: string, fields: seq[Field]) =
   # TODO: just add s, so fix feature.
@@ -49,7 +49,7 @@ function delete-capModelName-(id) {
     <ul>
   #for item in items(tabs):
     <li>
-      <a href=-pluralModelName-/${item["id"].str}>${item["name"].str}</a>
+      <a href=-pluralModelName-/${item["id"].str}>${item["-fieldName-"].str}</a>
       <a href=-pluralModelName-/${item["id"].str}/edit>Edit</a>
       <a href="javascript:delete-capModelName-(${item["id"].str});">Delete</a>
     </li>
@@ -63,6 +63,12 @@ function delete-capModelName-(id) {
   index_hear = index_hear.replace("-modelName-", modelName)
   index_hear = index_hear.replace("-pluralModelName-", pluralModelName)
   index_hear = index_hear.replace("-capModelName-", capModelName)
+
+  # TODO: support more than 2
+  var fieldName = fields[0].fieldName
+  # fields[0].fieldType
+  index_hear = index_hear.replace("-fieldName-", fieldName)
+
   index_template_fp.write(index_hear)
   index_template_fp.write("\n")
   defer: index_template_fp.close()
@@ -80,7 +86,7 @@ function delete-capModelName-(id) {
 function create-capModelName-() {
   var url = "../../-pluralModelName-";
   var data = {};
-  data.name = document.myform["name"].value;
+  data.-fieldName- = document.myform["-fieldName-"].value;
   var json = JSON.stringify(data);
   
   var xhr = new XMLHttpRequest();
@@ -101,7 +107,7 @@ function create-capModelName-() {
   <h1>New -modelName-</h1>
   <form name="myform" onSubmit="JavaScript:create-capModelName-()">
   <p>
-  Name: <input type="text" name="name">
+  -fieldName-: <input type="text" name="-fieldName-">
   </p>
   <p>
   <input type="submit" value="Submit">
@@ -115,6 +121,8 @@ function create-capModelName-() {
   new_hear = new_hear.replace("-modelName-", modelName)
   new_hear = new_hear.replace("-pluralModelName-", pluralModelName)
   new_hear = new_hear.replace("-capModelName-", capModelName)
+  new_hear = new_hear.replace("-fieldName-", fieldName)
+
   new_template_fp.write(new_hear)
   new_template_fp.write("\n")
   defer: new_template_fp.close()
@@ -133,7 +141,7 @@ function create-capModelName-() {
 function update-capModelName-(id) {
   var url = "../../-pluralModelName-/" + id;
   var data = {};
-  data.name = document.myform["name"].value;
+  data.-fieldName- = document.myform["-fieldName-"].value;
   var json = JSON.stringify(data);
 
   var xhr = new XMLHttpRequest();
@@ -154,7 +162,7 @@ function update-capModelName-(id) {
   <h1>Edit -modelName-</h1>
   <form name="myform" onSubmit="JavaScript:update-capModelName-(${tab["id"].str})">
   <p>
-  Name: <input type="text" name="name" value=${tab["name"].str}>
+  -fieldName-: <input type="text" name="-fieldName-" value=${tab["-fieldName-"].str}>
   </p>
   <p>
   <input type="submit" value="Submit">
@@ -169,6 +177,8 @@ function update-capModelName-(id) {
   edit_hear = edit_hear.replace("-modelName-", modelName)
   edit_hear = edit_hear.replace("-pluralModelName-", pluralModelName)
   edit_hear = edit_hear.replace("-capModelName-", capModelName)
+  edit_hear = edit_hear.replace("-fieldName-", fieldName)
+
   edit_template_fp.write(edit_hear)
   edit_template_fp.write("\n")
   defer: edit_template_fp.close()
@@ -187,11 +197,16 @@ function update-capModelName-(id) {
   <div id="menu">
     <ul>
     <li>Id: ${tab["id"].str}</li>
-    <li>Name: ${tab["name"].str}</li>
+    <li>-fieldName-: ${tab["-fieldName-"].str}</li>
     </ul>
   </div>
 </body>
   """
+
+  show_hear = show_hear.replace("-modelName-", modelName)
+  show_hear = show_hear.replace("-pluralModelName-", pluralModelName)
+  show_hear = show_hear.replace("-capModelName-", capModelName)
+  show_hear = show_hear.replace("-fieldName-", fieldName)
 
   # html  
   show_template_fp.write(show_hear)
@@ -236,7 +251,7 @@ routes:
   post "/-pluralModelName-":
     try:
       let j = parseJson(request.body)
-      var data: JsonNode = insert(j["name"].str)
+      var data: JsonNode = insert(j["-fieldName-"].str)
       resp $data, "application/json"
     except:
       resp Http400, "Unable to parse JSON payload"
@@ -244,7 +259,7 @@ routes:
     # Update
     try:
       let j = parseJson(request.body)
-      var data: JsonNode = update(@"id", j["name"].str)
+      var data: JsonNode = update(@"id", j["-fieldName-"].str)
       resp $data, "application/json"
     except:
       resp Http400, "Unable to parse JSON payload"
@@ -259,6 +274,7 @@ runForever()
   nim_hear = nim_hear.replace("-modelName-", modelName)
   nim_hear = nim_hear.replace("-pluralModelName-", pluralModelName)
   nim_hear = nim_hear.replace("-capModelName-", capModelName)
+  nim_hear = nim_hear.replace("-fieldName-", fieldName)
 
   # nim roure
   nim_fp.write(nim_hear)
@@ -277,7 +293,7 @@ let db = open("mydb.db","-modelName-","password","dbname")
 proc select(id: string): JsonNode =
   # select
   var row: Row = db.getRow(sql"select * from -modelName- where id = ?", id)
-  var data = %*{"id": row[0], "name": row[1]}
+  var data = %*{"id": row[0], "-fieldName-": row[1]}
   return data
 
 proc selectAll(): seq[JsonNode] =
@@ -285,18 +301,18 @@ proc selectAll(): seq[JsonNode] =
   var -pluralModelName-: seq[JsonNode]
   -pluralModelName- = @[]
   for row in rows:
-    -pluralModelName-.add(%*{"id": row[0], "name": row[1]})
+    -pluralModelName-.add(%*{"id": row[0], "-fieldName-": row[1]})
   # select
   return -pluralModelName-
 
 proc insert(name: string): JsonNode =
   var id = db.tryInsertId(sql"insert into -modelName- (name) values (?)", name)
-  var data = %*{"id": id, "name": name}
+  var data = %*{"id": id, "-fieldName-": name}
   return data
 
 proc update(id: string, name: string): JsonNode =
   db.exec(sql"update -modelName- set name = ? where id = ?", name, id)
-  var data = %*{"id": id, "name": name}
+  var data = %*{"id": id, "-fieldName-": name}
   return data
 
 proc delete(id: string): JsonNode =
@@ -320,6 +336,7 @@ proc create() =
   sc_hear = sc_hear.replace("-modelName-", modelName)
   sc_hear = sc_hear.replace("-pluralModelName-", pluralModelName)
   sc_hear = sc_hear.replace("-capModelName-", capModelName)
+  sc_hear = sc_hear.replace("-fieldName-", fieldName)
 
   sc_fp.write(sc_hear)
   sc_fp.write("\n")
@@ -354,7 +371,7 @@ if isGenerate and cnt > 2:
       var fieldType: string = arr[1]
 
       var field = Field(
-        name: name,
+        fieldName: name,
         fieldType: fieldType
       )
 
